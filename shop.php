6983b981c8ec60b0d9c18ad2c4fcd8b85b32d376
David MATHE <dmathe@e3r5p12.42.fr>
2016-10-09 12:00:40 +0200

    shop.php

---
+++ shop.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `db`
+//
+
+// `db`.`shop`
+$shop = array(
+  array('id' => '6','name' => 'Fruit du Gum Gum','description' => 'Rend ton corps élastique','price' => '5000','category_id' => '1','stock' => '3','image' => './img/fruit_du_gum_gum.jpg'),
+  array('id' => '7','name' => 'Fruit de la fragmentation','description' => 'Permet de séparer ton corps en plusieurs morceau','price' => '2000','category_id' => '1','stock' => '10','image' => './img/fruit_de_la_fragmentation.png'),
+  array('id' => '8','name' => 'Fruit de la girafe','description' => 'Rend ton corps à moitié girafe','price' => '2000','category_id' => '2','stock' => '8','image' => './img/fruit_de_la_girafe.jpg'),
+  array('id' => '9','name' => 'Fruit du léopard','description' => 'Rend ton corps à moitié léopard','price' => '6000','category_id' => '2','stock' => '3','image' => './img/fruit_du_leopard.jpg'),
+  array('id' => '10','name' => 'Fruit du feu','description' => 'Te permet de changer ton corps en feu','price' => '10000','category_id' => '3','stock' => '2','image' => './img/fruit_du_feu.png'),
+  array('id' => '11','name' => 'Fruit du sable','description' => 'Te permet de changer ton corps en sable','price' => '7000','category_id' => '3','stock' => '2','image' => './img/fruit_du_sable.png')
+);
